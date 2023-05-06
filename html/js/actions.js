8d2e04838957232f1aed366260b1371dc7b40f5b
Juan Alberto González <2136913+juanalbglz@users.noreply.github.com>
2023-05-06 12:09:33 +0000

    html test

---
+++ html/js/actions.js
@@ -0,0 +1,9 @@
+// Puedes agregar validaciones adicionales o enviar los datos del formulario a una base de datos.
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Aquí puedes agregar la lógica para enviar los datos del formulario a una base de datos o hacer cualquier otra acción necesaria.
+});
